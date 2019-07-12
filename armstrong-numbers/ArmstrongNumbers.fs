64fe66e2944e6919b56ba199e71b5a76c85fbf7b
Dood <E014392@aoins.com>
2019-07-11 22:04:54 -0400

    initial

---
+++ armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,18 @@
+﻿module ArmstrongNumbers
+
+let numDigits = function
+    | 0 -> 1.
+    | n -> n |> float |> log10 |> ceil
+
+let exp10 (x: int) = 10. ** (float x)
+
+let digiti (n: int) (i: int) =
+    (n / ((i - 1) |> exp10 |> int)) % 10 |> float
+
+let isArmstrongNumber (number: int) =
+    let d = number |> numDigits
+    let dgt = digiti number
+    let asum =
+        seq {(int d) .. -1 .. 1}
+        |> Seq.sumBy (fun i -> (i |> dgt) ** d)
+    int asum = number
