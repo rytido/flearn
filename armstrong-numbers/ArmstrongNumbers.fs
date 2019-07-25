ea42f45eb71a037092e4898848029f5411d3faa4
rytido <E014392@aoins.com>
2019-07-24 21:51:27 -0400

    completed

--- armstrong-numbers/ArmstrongNumbers.fs
+++ armstrong-numbers/ArmstrongNumbers.fs
@@ -4,15 +4,13 @@ let numDigits = function
     | 0 -> 1.
     | n -> n |> float |> log10 |> ceil
 
-let exp10 (x: int) = 10. ** (float x)
+let exp10 (x: int) = 10. ** (float x) |> int
 
 let digiti (n: int) (i: int) =
-    (n / ((i - 1) |> exp10 |> int)) % 10 |> float
+    (n / ((i - 1) |> exp10)) % 10 |> float
 
 let isArmstrongNumber (number: int) =
     let d = number |> numDigits
     let dgt = digiti number
-    let asum =
-        seq {(int d) .. -1 .. 1}
-        |> Seq.sumBy (fun i -> (i |> dgt) ** d)
+    let asum = seq {1 .. (int d)} |> Seq.sumBy (fun i -> (i |> dgt) ** d)
     int asum = number
